0ee39f3bc32cf3568f7473b7ea46e727e4254bf7
Andrew Muth <agmuth@gmail.com>
2023-11-22 14:57:25 -0700

    bfs and dfs working

--- flowprog/path_finding.py
+++ flowprog/path_finding.py
@@ -1,84 +1,54 @@
-from typing import Any
-from flowprog.graph import Graph, Node, Edge
-from collections import deque, defaultdict
-from typing import Tuple, Iterable, Any
+from collections import defaultdict, deque
+from typing import Any, Iterable
+
+from flowprog.graph import Edge, Graph, Node
 
 
 class BreadthFirstSearch:
     def __init__(self) -> None:
-        self.search_dequeue = deque()
-        self.visited_nodes = defaultdict(False)
-        
-    def _add_node_to_search_dequeue(self, edge: Edge) -> None:
-        self.search_dequeue.append(Edge)
-        
-    def _get_edge_from_search_dequeue(self) -> Tuple[Node, Node]:
-        return self.search_dequeue.popLeft()
-        
-    def _reconstruct_path(self, start: Node, end: Node) -> Iterable[Any]:
-        pass
-    
+        self.search_deque = deque()
+        self.visited_nodes = defaultdict(lambda: False)
+        self.curr_prev_node_mapping = defaultdict(None)
+
+    def _add_edge_to_search_deque(self, edge: Edge) -> None:
+        self.search_deque.append(edge)
+
+    def _get_edge_from_search_deque(self) -> Edge:
+        # FIFO
+        return self.search_deque.popleft()
+
+    def _reconstruct_path(self, start: Node, end: Node) -> Iterable[Node]:
+        path = [end]
+        while path[-1] != start:
+            path.append(self.curr_prev_node_mapping[path[-1]])
+        return path[::-1]
+
     def __call__(self, graph: Graph, start: Node, end: Node) -> Any:
-        self._add_edge_to_queue(Edge((None, start)))
-        
-        while len(self.search_dequeue) > 0:
-            edge_to_search_along = self._get_node_from_search_dequeue()
-            next_node = edge_to_search_along.nodes[1]
-            
-            if next_node == end:
-                break
-            if self.visited_nodes[next_node]:
+        self._add_edge_to_search_deque(Edge((None, start)))
+
+        while len(self.search_deque) > 0:
+            edge_to_search_along = self._get_edge_from_search_deque()
+            prev_node, curr_node = edge_to_search_along.nodes
+
+            if self.visited_nodes[curr_node]:
                 continue
-            
-            self.visited_nodes[next_node] = True
-            for edge in graph.edge_list[next_node]:
-                self._add_edge_to_search_dequeue(edge)
-                
+
+            self.visited_nodes[curr_node] = True
+            self.curr_prev_node_mapping[curr_node] = prev_node
+
+            if curr_node == end:
+                break
+
+            for edge in graph.edge_list[curr_node]:
+                self._add_edge_to_search_deque(edge)
+
         else:
             return list()  # no path
-        
+
         return self._reconstruct_path(start, end)
-            
-            
-            
-            
-        
-    
 
 
-if __name__ == "__main__":
-    node_a = Node(name="a")
-    node_b = Node(name="b")
-    node_c = Node(name="c")
-    node_d = Node(name="d")
-    node_e = Node(name="e")
-    node_f = Node(name="f")
-    node_g = Node(name="g")
-    node_h = Node(name="h")
-    
-    edge_a_b = Edge((node_a, node_b))
-    edge_a_c = Edge((node_a, node_c))
-    
-    edge_b_d = Edge((node_b, node_d))
-    edge_b_e = Edge((node_b, node_e))
-    
-    edge_c_f = Edge((node_c, node_f))
-    edge_c_g = Edge((node_c, node_g))
-    
-    edge_d_h = Edge((node_d, node_h))
-    
-    edge_list = [
-        edge_a_b,
-        edge_a_c,
-        edge_b_d,
-        edge_b_e,
-        edge_c_f,
-        edge_c_g,
-        edge_d_h,
-    ]
-    
-    graph = Graph(edge_list)
-    bfs = BreadthFirstSearch()
-    bfs(graph)
-    
-    
+class DepthFirstSearch(BreadthFirstSearch):
+    def _get_edge_from_search_deque(self) -> Edge:
+        # LIFO
+        return self.search_deque.pop()
---
+++ tests/test_path_finding.py
@@ -0,0 +1,51 @@
+from flowprog.graph import Edge, Graph, Node
+from flowprog.path_finding import BreadthFirstSearch, DepthFirstSearch
+
+node_a = Node(name="a")
+node_b = Node(name="b")
+node_c = Node(name="c")
+node_d = Node(name="d")
+node_e = Node(name="e")
+node_f = Node(name="f")
+node_g = Node(name="g")
+node_h = Node(name="h")
+
+edge_a_b = Edge((node_a, node_b))
+edge_a_c = Edge((node_a, node_c))
+
+edge_b_d = Edge((node_b, node_d))
+edge_b_e = Edge((node_b, node_e))
+
+edge_c_f = Edge((node_c, node_f))
+edge_c_g = Edge((node_c, node_g))
+
+edge_d_h = Edge((node_d, node_h))
+edge_g_h = Edge((node_g, node_h))
+
+edge_list = [
+    # (!) ordering here is important
+    edge_a_b,
+    edge_a_c,
+    edge_b_d,
+    edge_b_e,
+    edge_c_f,
+    edge_c_g,
+    edge_d_h,
+    edge_g_h,
+]
+
+graph = Graph(edge_list)
+
+
+def test_bfs():
+    bfs = BreadthFirstSearch()
+    bfs_path_found = bfs(graph, node_a, node_h)
+    bfs_path_correct = [node_a, node_b, node_d, node_h]
+    assert bfs_path_found == bfs_path_correct
+
+
+def test_dfs():
+    dfs = DepthFirstSearch()
+    dfs_path_found = dfs(graph, node_a, node_h)
+    dfs_path_correct = [node_a, node_c, node_g, node_h]
+    assert dfs_path_found == dfs_path_correct
